ea0a94ebbdd35d09846faa97db7512096e9b3325
xtrm <oss@xtrm.me>
2022-12-26 19:20:43 +0100

    chore: updated to burst rev/2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,35 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
-import fr.stardustenterprises.burst.meta.impl.pom.mit
+import enterprises.stardust.burst.preset.preset
+import enterprises.stardust.burst.preset.stardust.*
 
 plugins {
     alias(libs.plugins.burst)
 }
 
 burst {
+    preset<StardustPreset>()
+
     project {
         name = "burst-tests"
         description = "Burst testing grounds"
-        version = "4.0.0"
-
-        group = "fr.stardustenterprises.burst"
-        vendor = "Stardust Enterprises"
-
-        scm {
-            repositoryId = "stardust-enterprises/burst-tests"
-        }
+        version = "0.0.1-SNAPSHOT"
 
         pom {
-            licenses {
-                mit
-            }
-            developers {
-                "xtrm" { email = "oss@xtrm.me" }
-                "lambdagg"()
-            }
+            licenses { mit }
+            developers { xtrm; lambdagg }
         }
-    }.also { println(it) }
+        scm {
+            repositoryPath = "stardust-enterprises/burst-tests"
+        }
+    }
 
-//    features {
-//        kotlin
-//        java {
-//            compileVersion = JavaVersion.VERSION_1_8
-//        }
-//        dokka {
-//
-//        }
-//    }
+    features {
+        java {
+            javadoc = true
+            sources = true
+            jvmVersion = JavaLanguageVersion.of(8)
+        }
+        kotlin
+    }
 }
